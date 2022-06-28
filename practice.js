e73b2e8cb456d498006dbad27f2193236b6f31da
NokibulAlam <nokibul.alam02@gmail.com>
2022-06-28 14:43:10 +0600

    Filter method analized

--- practice.js
+++ practice.js
@@ -13,91 +13,91 @@
 
 /////////////////////////////////////////////////
 
-// SLICE method
-// let arr = ['a', 'b', 'c', 'd', 'e'];
-// console.log(arr.slice(2));
-// console.log(arr.slice(-2));
-// console.log(arr.slice(1, 4));
-// console.log(arr.slice(-1));
+SLICE method
+let arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr.slice(2));
+console.log(arr.slice(-2));
+console.log(arr.slice(1, 4));
+console.log(arr.slice(-1));
 
-// // SPLICE method
-// console.log(arr.splice(1));
-// console.log(arr);
+// SPLICE method
+console.log(arr.splice(1));
+console.log(arr);
 
-// // REVERSE 
-// const arr2 = ['j', 'i', 'h', 'g', 'f'];
-// console.log(arr2.reverse());
-// console.log(arr2);
+// REVERSE 
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
 
 
-// // CONCAT 
-// const letters = arr.concat(arr2);
-// console.log(letters);
-// console.log([...arr, ...arr2]);
+// CONCAT 
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
 
-// // JOIN
-// console.log(letters.join('-'));
+// JOIN
+console.log(letters.join('-'));
 
 // /************* forOf and forEach Loop **************/
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-// for(const [i, movement] of movements.entries()) {
-//   if(movement > 0) console.log(`Movement ${i + 1}: You deposited ${movement}`);
-//   else console.log(`Movement ${i + 1}: You withdrew ${movement}`)
-// }
+for(const [i, movement] of movements.entries()) {
+  if(movement > 0) console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  else console.log(`Movement ${i + 1}: You withdrew ${movement}`)
+}
 
-// /*
-// There is no way we can BREAK or CONTINUE ForEach Loop
-//  */
-// movements.forEach(function(mov, i, arr){
-//   if(mov > 0) console.log(`Movement ${i + 1}: You deposited ${mov}`);
-//   else console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
-// });
+/*
+There is no way we can BREAK or CONTINUE ForEach Loop
+ */
+movements.forEach(function(mov, i, arr){
+  if(mov > 0) console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  else console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+});
 
-// // 0: function(200)
-// // 1: function(450)
-// // 2: function(400)
-// // ...
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ...
 
 // /************* forOf and forEach Loop with Maps and Sets **************/
 
-// const currencies = new Map([
-//   ['USD', 'United States dollar'],
-//   ['EUR', 'Euro'],
-//   ['GBP', 'Pound sterling'],
-// ]);
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
 
-// currencies.forEach(function(value, key, map){
-//   console.log(`${key}: ${value}`);
-// });
+currencies.forEach(function(value, key, map){
+  console.log(`${key}: ${value}`);
+});
 
-// // SET
+// SET
 
-// const currenciesUnique = new Set(['USD', 'EURO', 'GBP', 'USD', 'EURO', 'GBP']);
-// console.log(currenciesUnique);
-// currenciesUnique.forEach(function(value, key, map){
-//   console.log(`${key}: ${value}`);
-// });
+const currenciesUnique = new Set(['USD', 'EURO', 'GBP', 'USD', 'EURO', 'GBP']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function(value, key, map){
+  console.log(`${key}: ${value}`);
+});
 
 
 /************* Coding Challange - 01 **************/
 
-// const dogsJulia = [3, 5, 2, 12, 7];
-// const dogsKate = [4, 1, 15, 8, 3];
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3];
 
-// dogsJulia.splice(-2);
-// // console.log(dogsJulia);
+dogsJulia.splice(-2);
+// console.log(dogsJulia);
 
-// const checkDogs = function(dogsJ, dogsK) {
-//   ages = dogsJ.concat(dogsK);
+const checkDogs = function(dogsJ, dogsK) {
+  ages = dogsJ.concat(dogsK);
 
-//   ages.forEach(function(age, i, arr) {
-//     age > 3 ? console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`) : console.log(`Dog number ${i + 1} is a puppy, and is ${age} years old`)
-//   });
-// }
+  ages.forEach(function(age, i, arr) {
+    age > 3 ? console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`) : console.log(`Dog number ${i + 1} is a puppy, and is ${age} years old`)
+  });
+}
 
-// checkDogs(dogsJulia, dogsKate);
+checkDogs(dogsJulia, dogsKate);
 
 
 
@@ -138,3 +138,21 @@ const movDes = movements.map((mov, i) =>
 );
 
 console.log(movDes);
+
+
+
+/************* FILTER METHOD **************/
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const dep = movements.filter(function(mov) {
+  return mov > 0;
+});
+console.log(dep);
+
+/* Arrow Functions */
+const deposit = movements.filter(mov =>  mov > 0);
+console.log(deposit);
+
+const withdrawal = movements.filter(mov => mov < 0);
+console.log(withdrawal);
