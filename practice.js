34d8b1e2fd7a5731d993f686273de3f3831597a5
NokibulAlam <nokibul.alam02@gmail.com>
2022-07-05 13:29:01 +0600

    updated method

--- practice.js
+++ practice.js
@@ -261,3 +261,12 @@ console.log(movements.includes(-130));
 // CONDITION
 const anyDeposits = movements.some(mov => mov > 0);
 console.log(anyDeposits);
+
+// EVERY METHOD
+console.log(account4.movements.every(mov => mov > 0));
+
+// Seperate Callback
+const deposit2 = mov => mov > 0;
+console.log(movements.every(deposit));
+console.log(movements.some(deposit));
+console.log(movements.filter(deposit));
