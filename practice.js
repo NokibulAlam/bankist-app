31fc92beacb070f94f1fc8f19eb5539f62ca61f4
NokibulAlam <nokibul.alam02@gmail.com>
2022-07-18 13:02:46 +0600

    wraping up the module

--- practice.js
+++ practice.js
@@ -302,3 +302,28 @@ console.log(movements);
 // });
 movements.sort((a, b) => b - a);
 console.log(movements);
+
+
+
+/*************** Creating and Filling Arrays ****************/
+const arr3 = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+const x = new Array(7);
+console.log(x);
+
+x.fill(1, 3, 5);
+console.log(x);
+
+arr3.fill(23, 2, 6);
+console.log(arr3);
+
+// Array.from
+const y = Array.from({ length: 7}, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7}, (_, i) => i + 1);
+console.log(z);
+
+const rollDice = Array.from({ length: 100}, (_, i) => Math.trunc(Math.random(i)));
+console.log(rollDice);
