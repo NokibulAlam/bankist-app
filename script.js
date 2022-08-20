e468e27feb1eb84525a52994956066393c408a98
NokibulAlam <nokibul.alam02@gmail.com>
2022-08-21 03:16:43 +0600

    Intl method added

--- script.js
+++ script.js
@@ -49,9 +49,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2022-07-27T17:01:17.194Z',
+    '2022-08-14T23:36:17.929Z',
+    '2022-08-17T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -106,6 +106,21 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const formatMovementDate = function(date) {
+  const countDays = (date1, date2) => Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+  const daysPassed = countDays(new Date(), date);
+
+  if(daysPassed === 0) return 'Today';
+  if(daysPassed === 1) return 'Yesterday';
+  if(daysPassed <= 7) return `${daysPassed} days ago`;
+
+  const day = `${date.getDate()}`.padStart(2, 0);
+  const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  const year = date.getFullYear();
+
+  return `${day}/${month}/${year}`;
+}
+
 
 const displayMovements = function(account, sort = false) {
   containerMovements.innerHTML = '';
@@ -117,12 +132,7 @@ const displayMovements = function(account, sort = false) {
   movs.forEach(function(mov, i){
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
-    const date = new Date(account.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(new Date(account.movementsDates[i]));
 
     const html = `
       <div class="movements__row">
@@ -184,12 +194,13 @@ const updateUI = function(account) {
 // Event Handler
 let currentAccount;
 
-
 // FAKE ALWAYS LOGIN
 currentAccount = account1;
 updateUI(currentAccount);
 containerApp.style.opacity = 100;
 
+
+// Experimenting INTERNATIONAL API
 
 btnLogin.addEventListener('click', function(e) {
   e.preventDefault();
@@ -206,13 +217,26 @@ btnLogin.addEventListener('click', function(e) {
 
       // Create current date & time
       const now = new Date();
-      const day = `${now.getDate()}`.padStart(2, 0);
-      const month = `${now.getMonth() + 1}`.padStart(2, 0);
-      const year = now.getFullYear();
-      const hours = `${now.getHours()}`.padStart(2, 0);
-      const min =` ${now.getMinutes()}`.padStart(2, 0);
+      const option = {
+        hour: 'numeric',
+        minute: 'numeric',
+        day: 'numeric',
+        month: 'numeric',
+        year: 'numeric',
+        // weekday: 'long',
+      }
 
-      labelDate.textContent = `${day}/${month}/${year}, ${hours}:${min}`;
+      const locale = navigator.language;
+      labelDate.textContent = new Intl.DateTimeFormat(currentAccount.locale, option).format(now);
+
+      // const day = `${now.getDate()}`.padStart(2, 0);
+      // const month = `${now.getMonth() + 1}`.padStart(2, 0);
+      // const year = now.getFullYear();
+      // const hours = `${now.getHours()}`.padStart(2, 0);
+      // const min =`${now.getMinutes()}`.padStart(2, 0);
+
+      // labelDate.textContent = `${day}/${month}/${year}, ${hours}:${min}`;
+
 
       // Clear Input field
       inputLoginUsername.value =  inputLoginPin.value = '';
@@ -441,3 +465,6 @@ console.log(future.getSeconds());
 
 
 //////////////////**********************Operations With Dates******************////////////////////
+
+const future2 = new Date(2037, 2, 14);
+console.log(+future2);
